4e8c8aecd979066d46e257c1af7acd706bbc9bb8
Thomas <thomastv1122@gmail.com>
2023-08-14 16:59:40 +0100

    done 844

---
+++ 844.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return self.getActualStr(s) == self.getActualStr(t)
+
+    def getActualStr(self, txt):
+        arr = []
+        for i in range(len(txt)):
+            if txt[i] == '#' and i != 0:
+                arr = arr[:-1]
+            else:
+                arr.append(txt[i])
+        return ' '.join(arr)
+
+sol = Solution()
+res = sol.backspaceCompare("asb#c", "ad#c")
+print(res)
